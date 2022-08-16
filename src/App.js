b54461a5f1751a76a63c96c11776961e2e489fea
EstiTw <estitwitowork@gmail.com>
2022-08-16 10:39:27 +0300

    added files

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
 import NavBar from "./components/navbar";
 import Products from "./components/products";
 import Posts from "./components/posts";
@@ -13,19 +14,22 @@ class App extends Component {
     return (
       <div>
         <NavBar />
+        <div className="content">
+          <Switch>
+            <Route path="/products/:id" component={ProductDetails} />
+            <Route
+              path="/products"
+              render={(props) => <Products sortBy="newest" {...props} />}
+            />
+            <Route path="/posts/:year/:month" component={Posts} />
+            {/* <Route path="/posts" component={Posts} /> */}
+            <Route path="/admin" component={Dashboard} />
+            <Route path="/" component={Home} />
+          </Switch>
+        </div>
       </div>
     );
   }
 }
 
 export default App;
-
-// function App() {
-//   return (
-//     <div>
-//       <h2>Let's get started!</h2>
-//     </div>
-//   );
-// }
-
-// export default App;
