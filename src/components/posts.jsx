36c2fda4f575d6d017ee76eacbfbf789e7fe03cf
EstiTw <estitwitowork@gmail.com>
2022-08-16 13:36:30 +0300

    optionals parameters, query string parameters, redirects, programmatic navigation, nested routing

--- src/components/posts.jsx
+++ src/components/posts.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import queryString from "query-string";
 
-const Posts = () => {
+const Posts = ({ match, location }) => {
+  const { month, year } = match.params;
+  // this will give us an object with properties based on parameters in the query string
+  const result = queryString.parse(location.search);
+  console.log(result); // {approved: 'true', sortBy: 'newest'}
   return (
     <div>
       <h1>Posts</h1>
-      Year: , Month:
+      Year:{year} , Month:{month}
     </div>
   );
 };
